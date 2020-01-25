d775b16ca00a3ae28c0564dc28103d68b214343b
antonlnd <antonland@protonmail.com>
2020-01-25 14:04:32 -0500

    hashtable

--- ex1.jsx
+++ ex1.jsx
@@ -1,18 +1,48 @@
-const args = process.argv.slice(2)
+const args = process.argv.slice(2);
+// time complexity o(n2) ; space: o(1)
+let arz = args[0]
+		.trim()
+		.split(',')
+		.map(v => Number(v)),
+	weiz = args[1],
+	funk = args[2];
 
-let arz = args[0].trim().split(',').map(v => Number(v)) , weiz = args[1];
 const findWeight = (arr, wei) => {
-	const sum = arr.reduce((start,val) => start+=val)
+	console.time('O2');
 	arr.forEach(item => {
-		let newArr = arr
-		newArr.splice(arr.indexOf(item),1)
+		let newArr = arr;
+		newArr.splice(arr.indexOf(item), 1);
 		newArr.forEach(sec => {
-			if((Number(sec) + Number(item)) == wei)
-				return console.log(sec , item, ":" , wei)	 
+			if (Number(sec) + Number(item) == wei) {
+				console.log(sec, item, ':', wei);
+				console.timeEnd('O2');
+				return;
+			}
+		});
+	});
+	return -1;
+};
 
-		})
-	})
-	return -1
-}
-
-findWeight(arz, weiz)
+// O(n) and linear
+const findSum = (arr, wei) => {
+	let hashTable = {};
+	console.time('hash');
+	arr.forEach((val, idx) => {
+		let diff = wei - val;
+		if (hashTable[val] != undefined) {
+			console.log('Found EL ', val, diff, '\nFind Time: ');
+			console.timeEnd('hash');
+			return [val, hashTable[wei - val]];
+		} else hashTable[diff] = idx;
+	});
+	return -1;
+};
+console.log(args);
+funk == '-hashtable'
+	? findSum(arz, weiz)
+	: funk == '-o2'
+	? findWeight(arz, weiz)
+	: console.log(
+			'only options -hashtable or -o2 allowed as last arg \n comma seperated array string in first arg, sum search integer as second ',
+	  );
+//findWeight(arz, weiz){ "keys": ["ctrl+alt+f"], "command": "js_prettier" }
